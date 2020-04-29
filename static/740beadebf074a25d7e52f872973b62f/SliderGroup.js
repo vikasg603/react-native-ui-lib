54c9d02c3aee88c639f3687d54f4ad9c66aef326
Ethan Sharabi <ethans@wix.com>
2020-04-30 01:03:22 +0300

    Updates

---
+++ static/740beadebf074a25d7e52f872973b62f/SliderGroup.js
@@ -0,0 +1,57 @@
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import SliderContext from './SliderContext';
+import {Colors} from '../../../style';
+import View from '../../view';
+
+
+export default class SliderGroup extends Component {
+  static propTypes = {
+    color: PropTypes.string,
+    onValueChange: PropTypes.func
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: Colors.getHSL(props.color)
+    };
+  }
+
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   if (nextProps.color !== Colors.getHexString(prevState.value)) {
+  //     return {value: Colors.getHSL(nextProps.color)};
+  //   }
+  //   return null;
+  // }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.color !== Colors.getHexString(this.state.value)) {
+      return {value: Colors.getHSL(nextProps.color)};
+    }
+  }
+
+  getContextProviderValue() {
+    return {
+      value: this.state.value, 
+      setValue: this.setValue
+    };
+  }
+
+  setValue = (value) => {
+    this.setState({value});
+    _.invoke(this.props, 'onValueChange', Colors.getHexString(value));
+  }
+
+  render() {
+    return (
+      <View {...this.props}>
+        <SliderContext.Provider value={this.getContextProviderValue()}>
+          {this.props.children}
+        </SliderContext.Provider>
+      </View>
+    );
+  }
+}
