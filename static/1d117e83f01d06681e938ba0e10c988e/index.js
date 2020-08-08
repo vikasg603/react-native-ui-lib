03e99ea3b8b6c8a2814913e4483ac356dd98bc3e
Ethan Sharabi <ethans@wix.com>
2020-08-08 07:36:17 +0300

    Updates

---
+++ static/1d117e83f01d06681e938ba0e10c988e/index.js
@@ -0,0 +1,366 @@
+// TODO: deprecate value allowing passing an object, separate between label and value props
+// TODO: extract picker labels from children in order to obtain the correct label to render (similar to NativePicker)
+
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import React from 'react';
+import {BaseComponent} from '../../commons';
+import View from '../../components/view';
+import Modal from '../modal';
+import Button from '../../components/button';
+import {TextField} from '../inputs';
+import * as PickerPresenter from './PickerPresenter';
+import NativePicker from './NativePicker';
+import PickerModal from './PickerModal';
+import PickerItem from './PickerItem';
+import {Colors} from '../../style';
+
+const PICKER_MODES = {
+  SINGLE: 'SINGLE',
+  MULTI: 'MULTI'
+};
+const ItemType = PropTypes.shape({
+  value: PropTypes.any,
+  label: PropTypes.string
+});
+
+/**
+ * @description: Picker Component, support single or multiple selection, blurModel and nativePicker
+ * @gif: https://media.giphy.com/media/3o751SiuZZiByET2lq/giphy.gif, https://media.giphy.com/media/TgMQnyw5grJIDohzvx/giphy.gif, https://media.giphy.com/media/5hsdmVptBRskZKn787/giphy.gif
+ * @example: https://github.com/wix/react-native-ui-lib/blob/master/demo/src/screens/componentScreens/FormScreen.js
+ */
+class Picker extends BaseComponent {
+  static displayName = 'Picker';
+  static propTypes = {
+    ...TextField.propTypes,
+    /**
+     * Picker current value in the shape of {value: ..., label: ...}, for custom shape use 'getItemValue' prop
+     */
+    value: PropTypes.oneOfType([
+      ItemType,
+      PropTypes.arrayOf(ItemType),
+      PropTypes.object,
+      PropTypes.string,
+      PropTypes.number
+    ]),
+    /**
+     * Callback for when picker value change
+     */
+    onChange: PropTypes.func,
+    /**
+     * SINGLE mode or MULTI mode
+     */
+    mode: PropTypes.oneOf(Object.keys(PICKER_MODES)),
+    /**
+     * Adds blur effect to picker modal (iOS only)
+     */
+    enableModalBlur: PropTypes.bool,
+    /**
+     * Render custom picker - input will be value (see above)
+     * Example:
+     * renderPicker = (selectedItem) => {...}
+     */
+    renderPicker: PropTypes.elementType,
+    /**
+     * Render custom picker item
+     */
+    renderItem: PropTypes.elementType,
+    /**
+     * Render custom picker modal (e.g ({visible, children, toggleModal}) => {...})
+     */
+    renderCustomModal: PropTypes.elementType,
+    /**
+     * Custom picker props (when using renderPicker, will apply on the button wrapper)
+     */
+    customPickerProps: PropTypes.object,
+    /**
+     * Add onPress callback for when pressing the picker
+     */
+    onPress: PropTypes.func,
+    /**
+     * A function that extract the unique value out of the value prop in case value has a custom structure
+     */
+    getItemValue: PropTypes.func,
+    /**
+     * A function that returns the label to show for the selected Picker value
+     */
+    getLabel: PropTypes.func,
+    /**
+     * The picker modal top bar props
+     */
+    topBarProps: PropTypes.shape(Modal.TopBar.propTypes),
+    /**
+     * Show search input to filter picker items by label
+     */
+    showSearch: PropTypes.bool,
+    /**
+     * Style object for the search input (only when passing showSearch)
+     */
+    searchStyle: PropTypes.shape({
+      color: PropTypes.string,
+      placeholderTextColor: PropTypes.string,
+      selectionColor: PropTypes.string
+    }),
+    /**
+     * Placeholder text for the search input (only when passing showSearch)
+     */
+    searchPlaceholder: PropTypes.string,
+    /**
+     * callback for picker modal search input text change (only when passing showSearch)
+     */
+    onSearchChange: PropTypes.func,
+    /**
+     * Render custom search input (only when passing showSearch)
+     */
+    renderCustomSearch: PropTypes.elementType,
+    /**
+     * Allow to use the native picker solution (different style for iOS and Android)
+     */
+    useNativePicker: PropTypes.bool,
+    /**
+     * Callback for rendering a custom native picker inside the dialog (relevant to native picker only)
+     */
+    renderNativePicker: PropTypes.elementType,
+    /**
+     * Pass props to the list component that wraps the picker options (allows to control FlatList behavior)
+     */
+    listProps: PropTypes.object
+  };
+
+  static defaultProps = {
+    ...TextField.defaultProps,
+    mode: PICKER_MODES.SINGLE
+  };
+
+  static modes = PICKER_MODES;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: props.value,
+      selectedItemPosition: 0,
+      items: this.extractPickerItems(props)
+    };
+
+    if (props.mode === Picker.modes.SINGLE && Array.isArray(props.value)) {
+      console.warn('Picker in SINGLE mode cannot accept an array for value');
+    }
+    if (props.mode === Picker.modes.MULTI && !Array.isArray(props.value)) {
+      console.warn('Picker in MULTI mode must accept an array for value');
+    }
+
+    // TODO: this warning should be replaced by the opposite
+    // we should warn user NOT to pass an object to the value prop
+    // if (props.useNativePicker && _.isPlainObject(props.value)) {
+    //   console.warn('UILib Picker: don\'t use object as value for native picker, use either string or a number');
+    // }
+  }
+
+  UNSAFE_componentWillReceiveProps(nexProps) {
+    this.setState({
+      value: nexProps.value
+    });
+  }
+
+  getAccessibilityInfo() {
+    const {placeholder} = this.props;
+    return {
+      accessibilityLabel: this.getLabel() ? `${placeholder}. selected. ${this.getLabel()}` : `Select ${placeholder}`,
+      accessibilityHint: this.getLabel()
+        ? 'Double tap to edit'
+        : `Goes to ${placeholder}. Suggestions will be provided`,
+      ...this.extractAccessibilityProps()
+    };
+  }
+
+  extractPickerItems(props) {
+    const {children} = props;
+    const items = React.Children.map(children, child => ({value: child.props.value, label: child.props.label}));
+    return items;
+  }
+
+  getLabel() {
+    const {value} = this.state;
+
+    if (_.isArray(value)) {
+      return _.chain(value)
+        .map('label')
+        .join(', ')
+        .value();
+    }
+
+    const {getLabel} = this.props;
+    if (_.isFunction(getLabel)) {
+      return getLabel(value);
+    }
+
+    if (_.isPlainObject(value)) {
+      return _.get(value, 'label');
+    }
+
+    // otherwise, extract from picker items
+    const {items} = this.state;
+    const selectedItem = _.find(items, {value});
+    return _.get(selectedItem, 'label');
+  }
+
+  handlePickerOnPress = () => {
+    this.toggleExpandableModal(true);
+    _.invoke(this.props, 'onPress');
+  };
+
+  toggleExpandableModal = value => {
+    this.setState({showExpandableModal: value});
+  };
+
+  toggleItemSelection = item => {
+    const {value} = this.state;
+    const newValue = _.xorBy(value, [item], 'value');
+    this.setState({
+      value: newValue
+    });
+  };
+
+  cancelSelect = () => {
+    this.setState({
+      value: this.props.value
+    });
+    this.toggleExpandableModal(false);
+    _.invoke(this.props, 'topBarProps.onCancel');
+  };
+
+  onDoneSelecting = item => {
+    this.setState({searchValue: '', value: item}); // clean search when done selecting
+    this.toggleExpandableModal(false);
+    _.invoke(this.props, 'onChange', item);
+  };
+
+  onSearchChange = searchValue => {
+    this.setState({
+      searchValue
+    });
+    _.invoke(this.props, 'onSearchChange', searchValue);
+  };
+
+  onSelectedItemLayout = ({
+    nativeEvent: {
+      layout: {y}
+    }
+  }) => {
+    this.setState({selectedItemPosition: y});
+  };
+
+  appendPropsToChildren = () => {
+    const {children, mode, getItemValue, showSearch, renderItem} = this.props;
+    const {value, searchValue} = this.state;
+    const childrenWithProps = React.Children.map(children, child => {
+      const childValue = PickerPresenter.getItemValue({getItemValue, ...child.props});
+      const childLabel = PickerPresenter.getItemLabel({...child.props, getLabel: child.props.getItemLabel});
+
+      if (!showSearch || _.isEmpty(searchValue) || _.includes(_.lowerCase(childLabel), _.lowerCase(searchValue))) {
+        const selectedValue = PickerPresenter.getItemValue({value, getItemValue});
+        const isSelected = PickerPresenter.isItemSelected(childValue, selectedValue);
+        return React.cloneElement(child, {
+          isSelected,
+          onPress: mode === Picker.modes.MULTI ? this.toggleItemSelection : this.onDoneSelecting,
+          getItemValue: child.props.getItemValue || getItemValue,
+          onSelectedLayout: this.onSelectedItemLayout,
+          renderItem: child.props.renderItem || renderItem,
+          accessibilityState: isSelected ? {selected: true} : undefined,
+          accessibilityHint: 'Double click to select this suggestion'
+        });
+      }
+    });
+
+    return childrenWithProps;
+  };
+
+  renderExpandableModal = () => {
+    const {
+      mode,
+      enableModalBlur,
+      topBarProps,
+      showSearch,
+      searchStyle,
+      searchPlaceholder,
+      renderCustomSearch,
+      renderCustomModal,
+      listProps,
+      testID
+    } = this.getThemeProps();
+    const {showExpandableModal, selectedItemPosition} = this.state;
+    const children = this.appendPropsToChildren(this.props.children);
+
+    if (renderCustomModal) {
+      const modalProps = {
+        visible: showExpandableModal,
+        toggleModal: this.toggleExpandableModal,
+        onSearchChange: this.onSearchChange,
+        children
+      };
+
+      return renderCustomModal(modalProps);
+    }
+
+    return (
+      <PickerModal
+        testID={`${testID}.modal`}
+        visible={showExpandableModal}
+        scrollPosition={selectedItemPosition}
+        enableModalBlur={enableModalBlur}
+        topBarProps={{
+          ...topBarProps,
+          onCancel: this.cancelSelect,
+          onDone: mode === Picker.modes.MULTI ? () => this.onDoneSelecting(this.state.value) : undefined
+        }}
+        showSearch={showSearch}
+        searchStyle={searchStyle}
+        searchPlaceholder={searchPlaceholder}
+        onSearchChange={this.onSearchChange}
+        renderCustomSearch={renderCustomSearch}
+        listProps={listProps}
+      >
+        {children}
+      </PickerModal>
+    );
+  };
+
+  render() {
+    const {useNativePicker, renderPicker, customPickerProps, testID} = this.props;
+
+    if (useNativePicker) {
+      return <NativePicker {...this.props}/>;
+    }
+
+    if (_.isFunction(renderPicker)) {
+      const {value} = this.state;
+      return (
+        <View left>
+          <Button {...customPickerProps} link onPress={this.handlePickerOnPress} testID={testID}>
+            {renderPicker(value)}
+          </Button>
+          {this.renderExpandableModal()}
+        </View>
+      );
+    }
+
+    const textInputProps = TextField.extractOwnProps(this.getThemeProps());
+    const label = this.getLabel();
+    return (
+      <TextField
+        color={Colors.dark10}
+        {...textInputProps}
+        {...this.getAccessibilityInfo()}
+        importantForAccessibility={'no-hide-descendants'}
+        value={label}
+        expandable
+        renderExpandable={this.renderExpandableModal}
+        onToggleExpandableModal={this.toggleExpandableModal}
+      />
+    );
+  }
+}
+
+Picker.Item = PickerItem;
+export default Picker;
---
+++ static/ee1a89e4040169c2549942a9c0960bf2/PickerDialog.ios.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+import {BaseComponent} from '../../commons';
+import Dialog from '../dialog';
+import View from '../view';
+import Text from '../text';
+import {Colors} from '../../style';
+import {WheelPicker} from '../../nativeComponents';
+
+class PickerDialog extends BaseComponent {
+  static propTypes = {
+    selectedValue: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    onValueChange: PropTypes.func,
+    onDone: PropTypes.func,
+    onCancel: PropTypes.func,
+    topBarProps: PropTypes.object,
+    children: PropTypes.array
+  };
+
+  state = {};
+
+  renderHeader() {
+    const {onDone, onCancel, topBarProps} = this.props;
+
+    return (
+      <View style={styles.header}>
+        <Text text70 blue30 onPress={onCancel} accessibilityRole={onCancel ? 'button' : undefined}>
+          {_.get(topBarProps, 'cancelLabel', 'Cancel')}
+        </Text>
+        <Text text70 blue30 onPress={onDone} accessibilityRole={onDone ? 'button' : undefined}>
+          {_.get(topBarProps, 'doneLabel', 'Done')}
+        </Text>
+      </View>
+    );
+  }
+
+  renderPicker() {
+    const {children, onValueChange, selectedValue, renderNativePicker} = this.props;
+    if (_.isFunction(renderNativePicker)) {
+      return renderNativePicker(this.props);
+    }
+    return (
+      <WheelPicker onValueChange={onValueChange} selectedValue={selectedValue}>
+        {children}
+      </WheelPicker>
+    );
+  }
+
+  render() {
+    const dialogProps = Dialog.extractOwnProps(this.props);
+    return (
+      <Dialog {...dialogProps} height={250} width="100%" migrate bottom animationConfig={{duration: 300}}>
+        <View flex bg-white>
+          {this.renderHeader()}
+          <View centerV flex>
+            {this.renderPicker()}
+          </View>
+        </View>
+      </Dialog>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 44,
+    backgroundColor: Colors.dark80,
+    paddingHorizontal: 17,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+});
+
+export default PickerDialog;
