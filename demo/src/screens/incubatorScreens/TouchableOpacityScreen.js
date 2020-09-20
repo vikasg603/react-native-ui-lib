b25bb9a5b8cb6442f0b7de1fafe303b739b646e1
Ethan Sharabi <ethans@wix.com>
2020-09-20 12:21:16 +0300

    Update example screen to reproduce reanimated issue

--- demo/src/screens/incubatorScreens/TouchableOpacityScreen.js
+++ demo/src/screens/incubatorScreens/TouchableOpacityScreen.js
@@ -1,59 +1,48 @@
-import React, {Component} from 'react';
-import {View, Text, Colors, Incubator} from 'react-native-ui-lib';
+import React, {Component, useCallback, useState} from 'react';
+import {View, Text, Colors, /* TouchableOpacity, */ Incubator} from 'react-native-ui-lib';
+import _ from 'lodash';
 
-class TouchableOpacityScreen extends Component {
-  state = {
-    counter: 0,
-    longPressCounter: 0
+const TouchableOpacity = Incubator.TouchableOpacity;
+
+/* Class  */
+// export default class Screen extends Component {
+//   state = {
+//     counter: 0
+//   };
+
+//   count = ({id}) => {
+//     this.setState({counter: this.state.counter + 1});
+//   };
+
+//   render() {
+//     console.warn('counter', this.state.counter);
+//     return (
+//       <View padding-page flex center>
+//         <Text>With Class Screen</Text>
+//         <TouchableOpacity padding-20 bg-orange30 marginB-10 onPress={this.count}>
+//           <Text>{this.state.counter}</Text>
+//         </TouchableOpacity>
+//       </View>
+//     );
+//   }
+// }
+
+/* Function */
+export default () => {
+  const [counter, setCounter] = useState(0);
+
+  const count = () => {
+    setCounter(counter + 1);
   };
 
-  onPress = () => {
-    this.setState({counter: this.state.counter + 1});
-  };
+  console.warn('counter', counter);
+  return (
+    <View padding-page flex center>
+      <Text>With Function</Text>
 
-  onLongPress = () => {
-    this.setState({longPressCounter: this.state.longPressCounter + 1});
-  };
-
-  renderExample(title, props) {
-    return (
-      <View row marginT-20 centerV>
-        <Text text70 marginR-20>
-          {title}
-        </Text>
-        <Incubator.TouchableOpacity
-          onPress={this.onPress}
-          onLongPress={this.onLongPress}
-          backgroundColor={Colors.blue30}
-          style={{alignItems: 'center', paddingHorizontal: 20, paddingVertical: 8, borderRadius: 50}}
-          activeOpacity={1}
-          {...props}
-        >
-          <Text white>Button</Text>
-        </Incubator.TouchableOpacity>
-      </View>
-    );
-  }
-
-  render() {
-    const {counter, longPressCounter} = this.state;
-    return (
-      <View bg-dark80 flex padding-20>
-        <Text text40>Native TouchableOpacity</Text>
-
-        <Text text50 center marginT-20>
-          COUNTER: {counter}
-        </Text>
-        <Text text50 center marginT-20>
-          LONG PRESS COUNTER: {longPressCounter}
-        </Text>
-
-        {this.renderExample('feedbackColor', {backgroundColor: Colors.red30, feedbackColor: Colors.red10})}
-        {this.renderExample('activeScale', {activeScale: 0.95})}
-        {this.renderExample('activeOpacity', {activeOpacity: 0.6})}
-      </View>
-    );
-  }
-}
-
-export default TouchableOpacityScreen;
+      <TouchableOpacity padding-20 bg-orange30 marginB-10 onPress={count}>
+        <Text>{counter}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
